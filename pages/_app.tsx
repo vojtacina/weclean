72f2726b53679fd1c42830b28c72f4cfc86911e5
vojta <vojta.cina@seznam.cz>
2023-03-09 20:44:54 +0100

    fix: theme color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <meta name="theme-color" content="#1E76DC" />
+          <meta name="theme-color" content="#f3f4f6" />
           <link rel="icon" href="/favicon.ico" />
           <link rel="manifest" href="/site.webmanifest" />
         </Head>
