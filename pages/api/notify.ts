e6a8a6fa9506fbe39fa77964cff1e3cf9bf5e2fe
Vojtěch Vojt <61758752+vojtacina@users.noreply.github.com>
2023-08-30 12:38:32 +0200

    Update notify.ts

--- pages/api/notify.ts
+++ pages/api/notify.ts
@@ -16,7 +16,7 @@ export default async function handler(
   sgMail.setApiKey(process.env.SENDGRID_API_KEY)
   
   const msg = {
-    to: process.env.SENDGRID_MAIL_TO, // Change to your recipient
+    to: process.env.SENDGRID_MAIL_TO, // Admin email
     from: 'info@focuscleaning.cz', // Change to your verified sender
     template_id: "d-215349e3e95249b493cef925bc8c89e8",
     dynamic_template_data: data
@@ -29,14 +29,14 @@ export default async function handler(
     })
 
   if(data?.contact.includes("@")) {
-    const msg = {
-      to: process.env.SENDGRID_MAIL_TO, // Change to your recipient
+    const msgToCustomer = {
+      to: data.contact, // Customer email
       from: 'info@focuscleaning.cz', // Change to your verified sender
       template_id: "d-2bcbd2fc0702416d85141f9fe9c888a6",
       dynamic_template_data: data
     }
 
-    await sgMail.send(msg).then(() => {
+    await sgMail.send(msgToCustomer).then(() => {
         console.log('Email to customer sent')
       }).catch((error:any) => {
         console.error(error)
