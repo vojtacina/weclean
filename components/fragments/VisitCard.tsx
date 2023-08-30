e81ed3a68cb46dea7a146c1e0dd5b023459837b0
Vojtěch Vojt <61758752+vojtacina@users.noreply.github.com>
2023-08-30 12:40:26 +0200

    Update VisitCard.tsx

--- components/fragments/VisitCard.tsx
+++ components/fragments/VisitCard.tsx
@@ -10,7 +10,7 @@ function VisitCard(props: Props) {
     return (
         <div className="border inline-flex items-center max-w-xs rounded-md border-gray-200 px-4 py-3">
             <div className="w-10 h-10 md:w-20 md:h-20 flex-shrink-0 rounded-full relative overflow-hidden">
-                <Image src={"/images/white-face.png"} blurDataURL={"/images/vojta.jpg"} alt="Fotka obličeje Viktora Ciny" fill className='object-cover w-full h-full'/>
+                <Image src={"/images/white-face.png"} blurDataURL={"/images/white-face.png"} alt="Fotka obličeje Viktora Ciny" fill className='object-cover w-full h-full'/>
             </div>
             <div className="flex flex-col items-start pl-4 text-left">
                 <div className="font-medium">Viktor Cina</div>
